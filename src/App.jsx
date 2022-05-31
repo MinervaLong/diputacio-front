d982aa33d48bfd977e2e41f38000b7a3f145fa46
Bianca <biankflorit@gmail.com>
2022-05-31 12:48:29 +0200

    api random user + css header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Login from "./components/login/Login";
+import Player from "./components/player/Player";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <Player />
       <Login />
     </div>
   );
