d47970cf47d35bba5f6d7b0d88649640a1064f5d
Bianca <biankflorit@gmail.com>
2022-05-31 10:24:34 +0200

    cambio app.js a app.jsx y browser router en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Login from "./components/login/Login";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
 
       <Login />
     </div>
+    </BrowserRouter>
   );
 }
 
